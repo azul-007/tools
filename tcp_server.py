8b2d6c71043ca8f026c52eefc04b983fd5c8d67e
azul-007 <megamanx6@protonmail.com>
2019-04-16 06:43:51 -0400

    created handle_client function handle

--- tcp_server.py
+++ tcp_server.py
@@ -5,3 +5,13 @@ bind_ip   = "0.0.0.0"
 bind_port = 9999
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+
+server.bind((bind_ip,bind_port))
+
+server.listen(5)
+
+print "* Listening on %s:%d" %(bind_ip,bind_port)
+
+#client handling thread
+def handle_client(client_socket):
+	
