c2443b7631dfccf160c5813d39e195fb608087ec
azul-007 <megamanx6@protonmail.com>
2019-04-17 22:20:51 -0400

    created function headers

--- netdog.py
+++ netdog.py
@@ -12,3 +12,33 @@ execute				= ""
 target				= ""
 upload_destination  = ""
 port				= 0
+
+
+
+def usage():
+
+
+
+
+
+def main():
+
+
+
+
+
+def client_sender(buffer):
+
+
+
+
+def server_loop():
+
+
+
+
+def run_command(command):
+
+
+
+def client_handler():
