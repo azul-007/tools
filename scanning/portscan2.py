8eff88e08f21d6e9811c75e0ef37b18441705e02
azul-007 <megamanx6@protonmail.com>
2019-12-19 06:26:43 -0500

    Completed lesson 9

--- scanning/portscan2.py
+++ scanning/portscan2.py
@@ -4,3 +4,16 @@ from threading import *
 
 def main():
 	parser = optparse.OptionParser("Usage of program: " + "-h <target host> -p <taget ports>")
+	parser.add_option('-h', dest='tgthost', type='string', help='specfiy target host')
+	parser.add_option('-p', dest='tgtpost', type='string', help='specfiy target ports seperated by commas')
+	(options, args) = parser.parse_args()
+	tgtHost = options.tgthost
+	tgtports = str(options.tgtports).split(',')
+
+	if (tgthost == None) | (tgtports[0] == None):
+
+		print(parser.usage)
+		exit(0)
+
+	portScan(tgthost,tgtports)
+
