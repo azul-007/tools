0154fd992bcb975125475af80a06bbb6dbce3e1c
azul-007 <megamanx6@protonmail.com>
2019-12-18 22:34:19 -0500

    created scanner method

---
+++ scanning/portscan.py
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = "192.168.1.127"
+port = 443
+
+def portscanner(port):
+
+	if sock.connect_ex((host, port)):
+		print("Port %d is closed" % (port))
+	else:
+		print("Port %d is open" % (port))
+
+portscanner(port)
