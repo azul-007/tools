5da6a64062b492601a2a8e1b199cf468be604c40
azul-007 <megamanx6@protonmail.com>
2019-12-19 06:37:25 -0500

    Completed lesson 10

--- scanning/portscan2.py
+++ scanning/portscan2.py
@@ -2,6 +2,28 @@ from socket import *
 import optparse
 from threading import *
 
+
+def portScan(tgthost, tgtports):
+	try:
+		tgtIP = gethostbyname(tgthost)
+
+	except:
+		print 'Unknown host %s' '%tgthost'
+
+	try:
+		tgtname = gethostbyaddress(tgtIP)
+		print 'Results for: ' + tgtname[0]	
+
+	except:
+		print 'Results for: ' + tgtIP
+
+	setdefaulttimeout(1)
+
+	for port in tgtports:
+	    	t = Thread(target = connScan, args=(tgthost, int(tgtports)))
+	    	t.start()
+	
+
 def main():
 	parser = optparse.OptionParser("Usage of program: " + "-h <target host> -p <taget ports>")
 	parser.add_option('-h', dest='tgthost', type='string', help='specfiy target host')
@@ -12,8 +34,11 @@ def main():
 
 	if (tgthost == None) | (tgtports[0] == None):
 
-		print(parser.usage)
+		print parser.usage
 		exit(0)
 
 	portScan(tgthost,tgtports)
+
+if __name__='__main__':
+	main()
 
