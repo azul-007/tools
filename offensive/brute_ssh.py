e0c66dfee7351a94d942bbfdeff2839ac9aa0d3c
azul-007 <megamanx6@protonmail.com>
2019-12-24 10:24:58 -0500

    created connect function

---
+++ offensive/brute_ssh.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+import pexpect
+
+
+def connect(user,host,password):
+	ssh_newkey = 'Are you sure you want to continue connecting?'
+	connStr = 'ssh ' + user + '@' + host
+	child = pexpect.spawn(connStr)
+	ret = child.expect([pexpect.TIMEOUT, ssh_newkey, '[P|p]assword:'])
+
+	if ret == 0:
+		print ('[-] Error Connecting')
+		return
+
+	if ret == 1:
+		child.sendline('yes')
+		ret = child.expect([pexpect.TIMEOUT, '[P|p]assword:'])
+
+		if ret == 0:
+			print ('[-] Error connecting')
+			return
+
+	child.sendline(password)
+	child.expect(PROMPT)
+	return child
