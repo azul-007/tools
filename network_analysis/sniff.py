96e1f2c536a17d4787fa5bfc9bc12b8be07fe618
azul-007 <megamanx6@protonmail.com>
2020-01-21 20:48:43 -0500

    General commit

--- network_analysis/sniff.py
+++ network_analysis/sniff.py
@@ -19,11 +19,11 @@ def analyze_ip_header(data_recv):
 	ip_hrd = struct.unpack('!6H4s4s',data_recv[:20]) #Go to struct tables to learn the values of 6H4s4s and 6s6sh
 
 
-def anaylzer_ether_header(data_recv):
+def anaylze_ether_header(data_recv):
 
 	ip_bool = False
 
-	eth_hdr  = struct.unpack('!6s6sh', data_recv[:14])
+	eth_hdr  = struct.unpack('!6s6sH', data_recv[:14])
 	dest_mac = binascii.hexlify(eth_hdr[0])
 	src_mac  = binascii.hexlify(eth_hdr[1])
 	proto    = eth_hdr[2] >> 8
