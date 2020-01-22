61fe2ac7fca62ed20b0d74f4eb96782441b88251
azul-007 <megamanx6@protonmail.com>
2020-01-21 20:29:41 -0500

    Completed anaylze ethernet header function

--- network_analysis/sniff.py
+++ network_analysis/sniff.py
@@ -14,7 +14,12 @@ sock_created = False
 sniffer_socket = 0
 
 
-def anaylzer_ether_header(data_recv):
+def analyze_ip_header(data_recv):
+
+	ip_hrd = struct.unpack('!6H4s4s',data_recv[:20]) #Go to struct tables to learn the values of 6H4s4s and 6s6sh
+
+
+def anaylze_ether_header(data_recv):
 
 	ip_bool = False
 
@@ -25,6 +30,14 @@ def anaylzer_ether_header(data_recv):
 	data     = data_recv[14:]
 
 	print("___________________________ETHERNET HEADER________________________________")
+	print("Destination MAC: %s:%s:%s:%s:%s:%s" % dest_mac[0:2],dest_mac[2:4],dest_mac[4:6],dest_mac[6:8],dest_mac[8:10],dest_mac[10:12])
+	print("Source Mac: %s:%s:%s:%s:%s:%s" % src_mac[0:2],src_mac[2:4],src_mac[4:6],src_mac[6:8],src_mac[8:10],src_mac[10:12])
+	print("PROTOCL: %hu" % proto)
+
+	if proto == 0x08:
+	    ip_bool = True:
+
+	return data, ip_bool				
 
 
 def main()
@@ -35,3 +48,15 @@ def main()
 
     	sniffer_socket = socket.socket(socket.PR_PACKET, socket.SOCK_RAW, socket.htons(0x0003))
     	sock_created = True
+
+
+    data_recv = sniffer_socket.recv(2048)
+    os.system('clear')
+
+    data_recv, ip_bool = anaylzer_ether_header(data_recv)
+
+    if ip_bool:
+    	data_recv, tcp_udp = anaylzer_ether_header(data_recv)
+
+    else:
+    	return 
