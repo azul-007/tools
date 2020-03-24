0bce98388cb4516e95c38e0409d48f6adf5a916b
azul-007 <megamanx6@protonmail.com>
2020-03-24 06:10:26 -0400

    completed file

---
+++ network_analysis/ReadConfig/ip_file_valid.py
@@ -0,0 +1,32 @@
+
+#!/usr/bin/python3
+
+import os.path
+import sys
+
+#Checking IP address and content
+def ip_file_valid():
+
+	ip_file = input("Enter IP the name and path of file: ")
+
+	if os.path.isfile(ip_file) == True:
+
+		print("File is valid")
+
+	else:
+
+		print("File {} does not exist. Please try again".format(ip_file))
+		sys.exit()
+
+		#Read selected file
+		selected_ip_file = open(ip_file,'r')
+
+		#Read from beginning
+		selected_ip_file.seek(0)
+
+		#Read each line
+		ip_list = selected_ip_file.readlines()
+
+		selected_ip_file.close()
+
+		return ip_list
