6b6cb70741e34d15145f10c6d0dd9846936c98da
azul-007 <megamanx6@protonmail.com>
2020-03-24 14:33:43 -0400

    checking in

--- network_analysis/ReadConfig/ssh_conn.py
+++ network_analysis/ReadConfig/ssh_conn.py
@@ -1,5 +1,8 @@
-import os
+import re
 import sys
+import time
+import os.path
+import paramiko
 
 user_file = input("Enter user file name: ")
 
@@ -11,4 +14,18 @@ if os.path.isfile(user_file) == True:
 else:
 
 	print("File {} does not exist".format(user_file))
+	sys.exit()
+
+
+#Checking commands file
+cmd_file = input("Enter commands file name: ")
+
+#Verify the valide cmmands file
+if os.path.isfile(cmd_file) == True:
+
+	print("Command file is valid. Sending commands to devices.")
+
+else:
+
+	print("File {} does not exist.".format(cmd_file))
 	sys.exit()
