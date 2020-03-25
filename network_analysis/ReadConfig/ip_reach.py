77e4eeb5e3d8281ea526c1bb00b68c868f4e76f2
azul-007 <megamanx6@protonmail.com>
2020-03-25 13:07:52 -0400

    shell=True needed in subprocess.call

--- network_analysis/ReadConfig/ip_reach.py
+++ network_analysis/ReadConfig/ip_reach.py
@@ -8,7 +8,7 @@ def ip_reach(list):
 
 		ip = ip.strip("\n")
 
-		ping_reply = subprocess.call("ping %s -c 2" % (ip), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+		ping_reply = subprocess.call("ping {} -c ".format(ip), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
 
 		if ping_reply == 0:
 			print("{} is reachable".format(ip))
