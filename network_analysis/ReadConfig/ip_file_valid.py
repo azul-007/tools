9a4c1a79a2920d04e7758039b18615762164aa97
azul-007 <megamanx6@protonmail.com>
2020-03-25 10:49:58 -0400

    fixed things

--- network_analysis/ReadConfig/ip_file_valid.py
+++ network_analysis/ReadConfig/ip_file_valid.py
@@ -6,26 +6,29 @@ import sys
 #Checking IP address and content
 def ip_file_valid():
 
-	ip_file = input("Enter IP the name: ")
+    ip_file = input("Enter IP the name: ")
 
-	if os.path.isfile(ip_file) == True:
+    while(os.path.isfile(ip_file) == False):
 
-		print("File is valid")
+        #print("File is valid")
+        ip_file = input("File is invalid. Please try again: ")
 
-	else:
+    else:
 
-		print("File {} does not exist. Please try again".format(ip_file))
-		sys.exit()
+        #print("File {} does not exist. Please try again".format(ip_file))
+        #sys.exit()
+        print("File is valid")
+        
 
-		#Read selected file
-		selected_ip_file = open(ip_file,'r')
+    #Read selected file
+    selected_ip_file = open(ip_file,'r')
 
-		#Read from beginning
-		selected_ip_file.seek(0)
+    #Read from beginning
+    selected_ip_file.seek(0)
 
-		#Read each line
-		ip_list = selected_ip_file.readlines()
+    #Read each line
+    ip_list = selected_ip_file.readlines()
 
-		selected_ip_file.close()
+    selected_ip_file.close()
 
-		return ip_list
+    return ip_list
