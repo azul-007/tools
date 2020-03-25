42b5eef2b9df2b3e9308663f6757075793daecbd
azul-007 <megamanx6@protonmail.com>
2020-03-25 09:49:00 -0400

    changed ssh_connection to ssh_conn

--- network_analysis/ReadConfig/ssh_conn.py
+++ network_analysis/ReadConfig/ssh_conn.py
@@ -8,105 +8,105 @@ user_file = input("Enter user file name: ")
 
 #Verify the validity of the username/passwd file
 if os.path.isfile(user_file) == True:
-	
-	print("Username/Password file is valid")
+    
+    print("\nUsername/Password file is valid")
 
 else:
 
-	print("File {} does not exist".format(user_file))
-	sys.exit()
+    print("File {} does not exist".format(user_file))
+    sys.exit()
 
 
 #Checking commands file
-cmd_file = input("Enter commands file name: ")
+cmd_file = input("\nEnter commands file name: ")
 
 #Verify the valide cmmands file
 if os.path.isfile(cmd_file) == True:
 
-	print("Command file is valid. Sending commands to devices.")
+    print("Command file is valid. Sending commands to devices." + "\n")
 
 else:
 
-	print("File {} does not exist.".format(cmd_file))
-	sys.exit()
+    print("File {} does not exist.".format(cmd_file))
+    sys.exit()
 
 
 #Open SSHv2 connection to the device
-def ssh_connection(ip):
+def ssh_conn(ip):
 
-	global user_file
-	global cmd_file
+    global user_file
+    global cmd_file
 
-	#Create SSH connection
-	try:
+    #Create SSH connection
+    try:
 
-		#Define SSH params
-  		selected_user_file = open(user_file, 'r')
+        #Define SSH params
+        selected_user_file = open(user_file, 'r')
 
-  		#Starting from the beginning of the file
-  		selected_user_file.seek(0)
+        #Starting from the beginning of the file
+        selected_user_file.seek(0)
 
-  		#Reading username from beginning of the file
-  		username = selected_user_file.readlines()[0].split(',')[0].rstrip("\n")
+        #Reading username from beginning of the file
+        username = selected_user_file.readlines()[0].split(',')[0].rstrip("\n")
 
-  		#Start from the beginning of file...grab password
-		password = selected_user_file.readlines()[0].split(',')[1].rstrip("\n")
+        #Start from the beginning of file...grab password
+        password = selected_user_file.readlines()[0].split(',')[1].rstrip("\n")
 
-		#Log into switches
-		session = paramiko.SSHClient()
+        #Log into switches
+        session = paramiko.SSHClient()
 
-		#Allows auto-accepting unknown host keys
-		session.set_missing_host_key_policy(paramiko,AutoAddPolicy())
+        #Allows auto-accepting unknown host keys
+        session.set_missing_host_key_policy(paramiko,AutoAddPolicy())
 
-		#Connect to the device
-		session.connect(ip.rstrip("\n"), username=username, password=password)
+        #Connect to the device
+        session.connect(ip.rstrip("\n"), username=username, password=password)
 
-		#Start an interactive shell session on the router
-		connection = session.invoke_shell()
+        #Start an interactive shell session on the router
+        connection = session.invoke_shell()
 
-		#Setting terminal length for entire output 
-		connection.send("enable\n")
-		connection.send("terminal length")
-		time.sleep(1)
+        #Setting terminal length for entire output 
+        connection.send("enable\n")
+        connection.send("terminal length")
+        time.sleep(1)
 
-		#Entering global config mode
-		connection.send("\n")
-		connection.send("configure terminal")
-		time.sleep(1)
+        #Entering global config mode
+        connection.send("\n")
+        connection.send("configure terminal")
+        time.sleep(1)
 
-		#Open user selected file for reading
-		selected_cmd_file = open(cmd_file,'r')
+        #Open user selected file for reading
+        selected_cmd_file = open(cmd_file,'r')
 
-		#Starting from the beginning of the file
-		selected_cmd_file.seek(0)
+        #Starting from the beginning of the file
+        selected_cmd_file.seek(0)
 
-		#Writing each line in the file to the device
-		for each_line in selected_cmd_file.readlines():
+        #Writing each line in the file to the device
+        for each_line in selected_cmd_file.readlines():
 
-			connection.send(each_line + "\n")
-			time.sleep(2)
+            connection.send(each_line + "\n")
+            time.sleep(2)
 
-		#close user file
-		selected_user_file.close()
+        #close user file
+        selected_user_file.close()
 
-		#Close cmd file
-		selected_cmd_file.close()
+        #Close cmd file
+        selected_cmd_file.close()
 
-		#Checking command output for IOS 
-		router_output = connection.recv(65535)
+        #Checking command output for IOS 
+        router_output = connection.recv(65535)
 
-		if re.search(b"% Invalid input", router_output):
+        if re.search(b"% Invalid input", router_output):
 
-			print("There was at least one IOS syntax error on device {}".format(ip))
+            print("There was at least one IOS syntax error on device {}".format(ip))
 
-		else:
+        else:
 
-			print("DONE for device {}".format(ip))
+            print("DONE for device {}".format(ip))
 
-		print(str(router_output) + "\n")
+        print(str(router_output) + "\n")
 
-		#Closing connection
-		session.close()
+        #Closing connection
+        session.close()
 
-	except paramiko.AuthenticationException:
-		print("Invalid username or password. Closing program")
+    except paramiko.AuthenticationException:
+        print("Invalid username or password. Closing program")
