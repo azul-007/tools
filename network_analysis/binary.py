e74524bf252cf8722d81fa77f2b3390af3b79d0a
azul-007 <megamanx6@protonmail.com>
2020-03-28 18:18:45 -0400

    Update binary.py

--- network_analysis/binary.py
+++ network_analysis/binary.py
@@ -6,19 +6,16 @@
 def bin_num():	
 
 	for num in range(256):
-
+		
 		bin_a = bin(num)
 		bin_a = bin_a[2:] #Strip "0b"
-		
+
 		if len(bin_a) < 8:
 
 			diff = 8 - len(bin_a)
-		
 			add_zero = "0" * diff
-	
 			print("*", num, "\t", str(add_zero + bin_a))
 		else:
-
 			print("*", num, "\t", bin_a)
 		
 bin_num()
