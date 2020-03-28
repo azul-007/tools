92aadf9d1d8d17a344ff92375b45cdf8d92f7101
azul-007 <megamanx6@protonmail.com>
2020-03-28 06:50:43 -0400

    sleep after ping for 2 seconds

--- network_analysis/ReadConfig/ip_reach.py
+++ network_analysis/ReadConfig/ip_reach.py
@@ -1,18 +1,20 @@
 import sys
+import time
 import subprocess
 
 #Check IP reachability
 def ip_reach(list):
 
-	for ip in list:
+    for ip in list:
 
-		ip = ip.strip("\n")
-
-		ping_reply = subprocess.call("ping {} -c ".format(ip), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
-
-		if ping_reply == 0:
-			print("{} is reachable".format(ip))
-			continue
-		else:
-			print("{} is NOT reachable".format(ip))
-			sys.exit()
+        ip = ip.strip("\n")
+        
+        ping_reply = subprocess.call("ping {} -c ".format(ip), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
+        time.sleep(2)
+        
+        if ping_reply == 0:
+            print("{} is reachable".format(ip))
+            continue
+        else:
+            print("{} is NOT reachable".format(ip))
+            sys.exit()
