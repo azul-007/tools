604558ad76b21a6aa9c5bcb49301d5d7fa2d79ce
azul-007 <azul-007@protonmail.com>
2020-07-28 12:15:48 -0400

    Update boxscan.py

--- reconnaissance/boxscan.py
+++ reconnaissance/boxscan.py
@@ -28,9 +28,6 @@ def nmap():
 
 def nikto():
     os.system('nikto -host http://{0} > {1}.nikto.txt'.format(args.target,args.name))
-
-#def dirb():
-    #os.system('dirb http://{0} > {1}.dirb.txt'.format(args.target,args.name))
     
 def gobuster():
     os.system('gobuster dir -u http://{0} -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt > {1}.gobuster.txt'.format(args.target,args.name))
@@ -41,17 +38,14 @@ if __name__ == '__main__':
     p1 = Process(target=nmap)
     p1.start()
     print("Starting nmap...)
-    #nmap_start = process_time()
     
     p2 = Process(target=nikto)
     p2.start()
     print("Starting nikto...)
-    #nikto_start = process_time()
     
     p3 = Process(target=gobuster)
     p3.start()
     print("Starting gobuster...")
-    #gobuster_start = process_time()
     
                   
     p1.join()
